08c54e424964dcbcb6d8da73f99bcc2d19cbe250
Ricardo Erazo <erazo.14@hotmail.com>
2014-03-12 11:32:42 -0600

    Actualizado version 1.1

--- src/compiladores/Visual_Error.java
+++ src/compiladores/Visual_Error.java
@@ -8,7 +8,42 @@ package compiladores;
  *
  * @author Bochoa
  */
-public class Visual_Error {
-    
-    
-}
+ class Visual_Error implements java.lang.Comparable{
+	int line, column;
+	String error ;
+	Visual_Error(int l, int c, String e){
+		line = l;
+		column = c;
+		error = e;
+	}
+        
+          @Override
+    public int compareTo(Object o) {
+        Visual_Error comp = (Visual_Error) o;
+        if (line == -1 ){
+            return 1;
+        }else{
+            if (comp.line == -1){
+                return -1;
+            }
+        }
+        int result = 0 ;
+        if (line < comp.line){
+            result = -1;
+        }else{
+            if (line == comp.line){
+                if(column < comp.column){
+                    result = -1;
+                }else{
+                    if (column == comp.column)
+                        result = 0;
+                    else
+                        result = 1;
+                }
+            }else{
+                result = 1;
+            }
+        }
+        return result;
+    }
+ }
